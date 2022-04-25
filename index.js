bf9d4ee74f31a1e0798918c257cdfd61501edb92
soregashi-27 <tomo.soul.15@gmail.com>
2022-04-25 15:42:14 +0900

    typoを修正

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 //visible&invisible対象のelementを定義する
-const togglePassword = document.quertSelector('#togglePassword');
-const password = document.querySelector('#passeord');
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
 
 //クリックしたらパスワードを表示させる
-togglePassword.addEventLestener('click', function () {
+togglePassword.addEventListener('click', function () {
   const type =
-    password.getAttribute('type') === 'password' ? 'next' : 'password';
+    password.getAttribute('type') === 'password' ? 'text' : 'password';
   password.setAttribute('type', type);
 
   this.classList.toggle('bi-eye');
+});
+
+//form elementを定義する
+const form = document.querySelector('form');
+
+//クリックしたらパスワードを非表示
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
 });
